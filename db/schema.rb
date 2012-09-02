bfe5096dd997719f0200d17b80ba4bac8f0a2297
Tony Miller <mcfiredrill@gmail.com>
2012-09-02 13:12:56 -0700

    add the rest of the twitter omniauth stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120901224906) do
+ActiveRecord::Schema.define(:version => 20120902200356) do
 
   create_table "mojis", :force => true do |t|
     t.string   "content"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(:version => 20120901224906) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
