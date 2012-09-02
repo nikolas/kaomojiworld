d8575d6e1dade4c3a28daf511c7803dd00e646eb
Tony Miller <mcfiredrill@gmail.com>
2012-09-02 13:21:24 -0700

    email not username

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.provider = auth.provider
       user.uid = auth.uid
-      user.username = auth.info.nickname
+      user.email = auth.info.nickname
     end
   end
 
