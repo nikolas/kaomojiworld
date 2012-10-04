367d485105cc69fe818a91e4e35d34989a7672ec
Tony Miller <mcfiredrill@gmail.com>
2012-10-03 19:21:17 -0700

    new stylez

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ group :assets do
 
   gem 'uglifier', '>= 1.0.3'
   gem 'twitter-bootstrap-rails'
+
+  gem 'bourbon'
 end
 
 gem 'jquery-rails'
--- app/helpers/mojis_helper.rb
+++ app/helpers/mojis_helper.rb
@@ -1,6 +1,6 @@
 module MojisHelper
   def random_moji_link
     moji = Moji.first(:order => "RANDOM()") #postgresql
-    link_to moji.content.first(15), moji_path(moji)
+    link_to moji.content.first(15), moji_path(moji) if moji
   end
 end
