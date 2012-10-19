da5b0de94ecd2755f482c0a8e40208444c119266
Tony Miller <mcfiredrill@gmail.com>
2012-10-19 13:24:26 -0700

    fixes

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -2,7 +2,7 @@ class Favorite < ActiveRecord::Base
   belongs_to :user
   belongs_to :moji
 
-  validates_presence_of, :moji_id, :user_id
+  validates_presence_of :moji_id, :user_id
 
   attr_accessible :moji_id, :user_id
 end
