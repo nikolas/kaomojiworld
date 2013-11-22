fb414864e2426e7bb04e8c2bf85a487495489ea1
Tony Miller <mcfiredrill@gmail.com>
2013-11-22 22:00:02 +0900

    fix extract_locale_from_accept_language_header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
 
   private
   def extract_locale_from_accept_language_header
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    if request.env['HTTP_ACCEPT_LANGUAGE']
+      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    end
   end
 end
