97ab1977e93e337a82c1273df08a9e1f03ceed25
Tony Miller <mcfiredrill@gmail.com>
2014-03-23 22:46:00 -0700

    use env vars for secret_token and secret_key_base in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,10 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Kaomojiworld::Application.config.secret_token = 'c3050000262cdcdd4e6fa317fb3050b32e2ac83db9ec8f2ee3bb96f1fe4fa7b84489fb60b70b0edeea437c1f2302077ef1fbdfedc3071fc8a3e50687573babfd'
+if ENV['RAILS_ENV'] == 'production'
+  Kaomojiworld::Application.config.secret_token = ENV['SECRET_TOKEN']
+  Kaomojiworld::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+else
+  Kaomojiworld::Application.config.secret_token = 'c3050000262cdcdd4e6fa317fb3050b32e2ac83db9ec8f2ee3bb96f1fe4fa7b84489fb60b70b0edeea437c1f2302077ef1fbdfedc3071fc8a3e50687573babfd'
+  Kaomojiworld::Application.config.secret_key_base = '89328hdfgdfinb0n0h04htw0ef0eoodifoxioxibFFwfa7b84489fb60b70b0edeea437c1f2302077ef1fbdfedc3071fc8a3e50687573babfd'
+end
