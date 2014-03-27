c24428ffc5c952e317a331283ca7081d0d25be35
Tony Miller <mcfiredrill@gmail.com>
2014-03-26 18:39:10 -0700

    bigger mojis

--- app/helpers/mojis_helper.rb
+++ app/helpers/mojis_helper.rb
@@ -4,11 +4,14 @@ module MojisHelper
     link_to moji.content.first(15), moji_path(moji) if moji
   end
 
-  def tweet_text(moji)
+  def tweet_text moji
     if moji.user.provider == "twitter"
       "#{moji.content} - by @#{moji.user.username}"
     else
       "#{moji.content} - by #{moji.user.username}"
     end
+  end
+
+  def display_tags tags
   end
 end
